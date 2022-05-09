c7246284f6db041d0f97a9b0582dacfa6b20e5fb
joepamedia <josephpalmisano18@gmail.com>
2022-05-09 12:56:38 -0500

    added responsive nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,25 +4,29 @@ function Header() {
   return (
     <>
       {/* add flexbox */}
-      <header>
-        Joe Pa Media Updated
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/photos">Photos</Link>
-            </li>
-            <li>
-              <Link to="/videos">Videos</Link>
-            </li>
-            <li>
-              <Link to="/coding">Coding</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <section class="top-nav">
+        <div>
+          <h1>Joe Pa Media</h1>
+        </div>
+        <input id="menu-toggle" type="checkbox" />
+        <label class="menu-button-container" for="menu-toggle">
+          <div class="menu-button"></div>
+        </label>
+        <ul class="menu">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/photos">Photos</Link>
+          </li>
+          <li>
+            <Link to="/videos">Videos</Link>
+          </li>
+          <li>
+            <Link to="/coding">Coding</Link>
+          </li>
+        </ul>
+      </section>
     </>
   );
 }
